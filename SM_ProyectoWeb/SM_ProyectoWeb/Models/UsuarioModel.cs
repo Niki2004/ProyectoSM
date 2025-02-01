adaa250051a267623127b1dba8277ff872542dcd
Cosmo0004 <108553854+Cosmo0004@users.noreply.github.com>
2025-02-01 10:44:13 -0600

    Clase 03

---
+++ SM_ProyectoWeb/SM_ProyectoWeb/Models/UsuarioModel.cs
@@ -0,0 +1,14 @@
+﻿
+namespace SM_ProyectoWeb.Models
+{
+    public class UsuarioModel
+    {
+        public string? Identificacion { get; set; }
+
+        public string? Contrasenia { get; set; } // ? ya sé que nacen en nulo 
+
+        public string? Nombre { get; set; }
+
+        public string? Correo { get; set; }
+    }
+}
