c5e8989e4d3f338fdb843e7925903e84805e9399
Cosmo0004 <108553854+Cosmo0004@users.noreply.github.com>
2025-02-08 10:14:32 -0600

    Clase04

--- SM_ProyectoWeb/SM_ProyectoWeb/Controllers/LoginController.cs
+++ SM_ProyectoWeb/SM_ProyectoWeb/Controllers/LoginController.cs
@@ -1,24 +1,57 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SM_ProyectoWeb.Models;
 
 namespace SM_ProyectoWeb.Controllers
 {
     public class LoginController : Controller
     {
-        public IActionResult IniciarSesion()
+        private readonly IHttpClientFactory _httpClient;
+
+        public LoginController(IHttpClientFactory httpClient)
         {
-            return View();
+
+            _httpClient = httpClient;
+
         }
 
-        public IActionResult Principal()
-        {
-            return View();
-        }
-
+        #region RegistrarCuenta
+        [HttpGet]
         public IActionResult RegistrarCuenta()
         {
             return View();
         }
 
+        [HttpPost]
+        public IActionResult RegistrarCuenta(UsuarioModel model)
+        {
+            using (var api = _httpClient.CreateClient()) //Using es un delimirador se acceso
+            {
+               var url = "https://localhost:44357/api/Login/RegistrarCuenta";
+               var resul = api.PostAsJsonAsync(url, model).Result;
+
+                if (resul.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("IniciarSesion", "Login");
+                }
+            }
+
+            return View();
+        }
+        #endregion
+
+        [HttpGet]
+        public IActionResult IniciarSesion()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Principal()
+        {
+            return View();
+        }
+
+        [HttpGet]
         public IActionResult RecuperarContrasenia()
         {
             return View();
